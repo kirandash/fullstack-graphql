988399067dd703405657b8a6c730a2dc823c6ca2
kirandash <thekirankumardash@gmail.com>
2025-05-08 11:45:48 +0800

    3.1 Apollo Client & API Querying

---
+++ Notes.md
@@ -0,0 +1,5 @@
+## 3 Apollo React
+
+### 3.1 Apollo Client & API Querying
+
+- Use apollo client dev tools to see the flat structure of data in the "Cache" tab
--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,41 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
 
 /**
  * Create a new apollo client and export as default
  */
+const link = new HttpLink({
+  uri: "https://rickandmortyapi.com/graphql",
+});
+// cache is a new instance of InMemoryCache. This will be used to store the data that we fetch from the server.
+// Extensible InMemoryCache
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache,
+});
+
+const query = gql`
+  {
+    characters {
+      results {
+        name
+        id
+      }
+    }
+
+    characters {
+      results {
+        name
+        id
+      }
+    }
+  }
+`;
+
+client.query({ query }).then((result) => console.log(result));
+
+export default client;
